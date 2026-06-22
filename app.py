54946c41b89948973bc4f2bcadc6f8b6a218f271
Dharani <dharanir.sse@saveetha.com>
2026-06-22 11:10:33 +0530

    Third Updated version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return " Hello welcome to simats"
+    return " Hello welcome to software engineering class"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
