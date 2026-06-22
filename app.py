b38d11d4ce2545d7825ca17e622fe651f71aeca1
Dharani <dharanir.sse@saveetha.com>
2026-06-22 09:28:42 +0530

    Added GitHub Actions workflow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return " Hi Hello Students"
+    return " Hi Hello Students, hru?"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
